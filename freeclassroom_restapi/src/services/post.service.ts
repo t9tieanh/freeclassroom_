3e3e0d2b00eae7ec9aae5ce5157e486b4732b3e3
t9tieanh <phama9162@gmail.com>
2025-06-25 18:22:48 +0700

    update caching class-detail and post with redis

--- freeclassroom_restapi/src/services/post.service.ts
+++ freeclassroom_restapi/src/services/post.service.ts
@@ -2,6 +2,8 @@ import { Types } from 'mongoose'
 import { JwtPayloadDto } from '~/dto/request'
 import { CreationPostDto } from '~/dto/request/post.dto'
 import { PostModel } from '~/models'
+import Redis from '~/config/redis'
+import { NOTFOUND_CACHE, POST_CACHE } from '~/utils/constants'
 
 const createPost = async (newPostDto: CreationPostDto, user: JwtPayloadDto) => {
   const newPost = new PostModel({
@@ -22,9 +24,25 @@ const getPostsBySection = async (sectionId: string) => {
 }
 
 const findPostById = async (postId: string) => {
-  const post = await PostModel.findOne({
+  // thực hiện tìm trong cache
+  const client = Redis.getRedisClient()
+  const keyCache = `${POST_CACHE}${postId}`
+
+  let post: any = await client.get(keyCache)
+
+  // -> cache hit
+  if (post) return post
+
+  post = await PostModel.findOne({
     _id: new Types.ObjectId(postId)
   }).populate('createBy', 'email name image -_id')
+
+  if (!post) post = NOTFOUND_CACHE
+
+  // set lại vào cache ( không có trong database vẫn set cái này lại cho cache )
+  await client.set(keyCache, JSON.stringify(post), {
+    EX: 1800 // thời gian sống là 30 phút
+  })
 
   return post
 }
--- freeclassroom_restapi/src/utils/constants.ts
+++ freeclassroom_restapi/src/utils/constants.ts
@@ -1,1 +1,4 @@
 export const WHITELIST_DOMAINS = ['http://localhost:3000']
+export const CLASSROOM_CACHE: string = 'CLASSROOM:'
+export const POST_CACHE: string = 'POST:'
+export const NOTFOUND_CACHE = null
